678190e1433a5380c1e3e074c3c4354216d7547f
Gulsenem <og.durdyeva@gmail.com>
2021-06-30 12:44:50 +0200

    fertig

--- aufgabe1.js
+++ aufgabe1.js
@@ -1,29 +1,33 @@
 var inputText = document.getElementById("inputText");
-
+var liElemente = [];
 
 function add()
 {
+    if(inputText.value =="")
+    {return}
+
     var liste = document.getElementById("liste");
 
     var li = document.createElement("li");
-    li.innerHTML = inputText.value;
+    var span = document.createElement("span");
+    span.innerHTML = inputText.value;
 
     var divIcon = document.createElement("div");
     divIcon.classList.add("icons");
 
     var check_icon = document.createElement("i");
     check_icon.classList.add("far", "fa-check-square");
-    check_icon.onclick = function dekor()
+    check_icon.onclick = function ()
     {
-        li.childNodes[1].style.textDecoration = "line-through";
-        
+        span.style.textDecoration = "line-through";
+        li.style.color = "green";
         check_icon.style.color = "green";
     }
 
     var kreuz_icon = document.createElement("i");
     kreuz_icon.classList.add("far", "fa-times-square");
     kreuz_icon.style.color = "red";
-    kreuz_icon.onclick = function loschen()
+    kreuz_icon.onclick = function()
     {
         li.style.display = "none";
     }
@@ -31,6 +35,7 @@ function add()
 
 
     liste.appendChild(li);
+    li.appendChild(span);
     li.appendChild(divIcon);
     divIcon.appendChild(check_icon);
     divIcon.appendChild(kreuz_icon);
